5655182aebcf5fbae55f60e864b478519cf3ebf4
FredsTechVerse <alfredgithinji87@gmail.com>
2024-04-19 14:59:40 +0300

    bug:refresh fix

--- lib/userActions.ts
+++ lib/userActions.ts
@@ -74,6 +74,7 @@ export const findAllUsers = async () => {
     try {
         await connectMongoDB();
         const userData = await User.find()
+        revalidateTag("user");
         const response = {
             status: 200,
             message: "Users Found",
